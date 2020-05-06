20f7acc299eab00e242ba2c529db4561e29edc24
krishnab <cyclotomiq@gmail.com>
2020-05-06 11:14:56 -0400

    updated path to use joinpath() instead of relative path

--- src/model_demographic_inertia_gender.jl
+++ src/model_demographic_inertia_gender.jl
@@ -382,7 +382,8 @@ function update_text_column_names!(df)
 end
 
 function concatenate_actual_data_to_simulation!(df_simulation::DataFrame)
-    df_actual = CSV.read("./src/mgmt_data.csv")
+    df_actual = CSV.read(joinpath(@__DIR__, "mgmt_data.csv"))
+    #@bp
     if size(df_simulation)[1] > size(df_actual)[1]
         difference = size(df_simulation)[1] - size(df_actual)[1]
         missing_df = missings(difference, size(df_actual)[2]) |> DataFrame
